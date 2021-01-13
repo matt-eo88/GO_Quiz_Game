850a9d1423b25205520e1c7579f91214dfec98c7
Matteo Di Gregorio <matteodgrgr@gmail.com>
2021-01-13 20:03:04 +0000

    add capability to show question to user and check answers

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // The problem - (question/answer)
@@ -32,7 +33,23 @@ func main() {
 		exit("Failed to parse the provided csv file")
 	}
 
-	fmt.Println(lines)
+	problems := parseLines(lines)
+	printAndCheck(problems)
+}
+
+func printAndCheck(problems []problem) {
+	// Print questions to stdout and check answer
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
@@ -40,7 +57,7 @@ func parseLines(lines [][]string) []problem {
 	for i, line := range lines {
 		ret[i] = problem{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
