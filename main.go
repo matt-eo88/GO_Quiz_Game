4f2dca79ffcdda5b278c27698135b84cf02ce3b2
Matteo Di Gregorio <matteodgrgr@gmail.com>
2021-01-15 20:05:38 +0000

    add timer - 30secs to give an answer

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 // The problem - (question/answer)
@@ -18,6 +19,7 @@ func main() {
 	// Sets command line flags
 	csvFilename := flag.String("csv", "problems.csv",
 		"a csv file in the format 'question,answer'")
+	timelimit := flag.Int("Limit", 30, "The time limit for the quiz in seconds")
 	flag.Parse()
 
 	// Opens the file
@@ -34,18 +36,33 @@ func main() {
 	}
 
 	problems := parseLines(lines)
-	printAndCheck(problems)
+
+	timer := time.NewTimer(time.Duration(*timelimit) * time.Second)
+
+	printAndCheck(problems, timer)
 }
 
-func printAndCheck(problems []problem) {
+func printAndCheck(problems []problem, timer *time.Timer) {
 	// Print questions to stdout and check answer
 	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			correct++
+
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
 		}
 	}
 
